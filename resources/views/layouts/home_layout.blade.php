3af0f0b0e6411e078b2b3357dc53a8450e0d639d
Jon Gu <jon.gu@carney.co>
2024-05-31 17:49:17 -0600

    rm fonts

--- resources/views/layouts/home_layout.blade.php
+++ resources/views/layouts/home_layout.blade.php
@@ -8,7 +8,6 @@
         <title>Study Ukrainian @yield('title')</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">        @yield('css-links')
