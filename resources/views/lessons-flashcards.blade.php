23521f9a4ac55a88c2b948f0e12ce3c1eae854ec
Jon Gu <jon.gu@carney.co>
2024-06-10 12:13:01 -0500

    rename

--- resources/views/lessons-flashcards.blade.php
+++ resources/views/lessons-flashcards.blade.php
@@ -22,12 +22,12 @@
 @isset($flashcards)
     <div class="bg-light my-5 p-5 flex flex-col">
         <h2>Lesson 1 Flashcards</h2>
-        @if(Auth::check())
+        @auth()
             <div class="mt-2">To add a flashcard to your deck, click the <span style="color: green; padding-left: 3px; padding-right: 3px; border: 1px solid rgb(200, 200, 200); background-color:rgb(239, 239, 239);">+</span> button next to the phrase that you want to add as a flashcard.  <a href="/study-record/test">Study your flashcards now!</a></div>
         @else
             <div class="mt-3"><b>You are not logged in!</b></div>
             <div> <a href="/login">Log in</a> or <a href="/register">Create an Account</a> to add these flashcards to your collection.</div>
-        @endif
+        @endauth
         <hr>
 
         <ul>
