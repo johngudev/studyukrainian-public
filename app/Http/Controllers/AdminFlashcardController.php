a77e49365378c39151cacdb24611eed37f4ca5bf
Jon Gu <jon.gu@carney.co>
2024-06-23 11:08:27 -0500

    refactored all admin flashcard routes

--- app/Http/Controllers/AdminFlashcardController.php
+++ app/Http/Controllers/AdminFlashcardController.php
@@ -34,4 +34,38 @@ class AdminFlashcardController extends Controller
     
         return redirect('/admin/flashcard/all');
     }
+
+    public function store() {
+
+        if (Auth::user()->id != 1) {
+            return redirect('/');
+        }
+    
+        $flashcard = new Flashcard;
+    
+        $flashcard->english_phrase = request()->english_phrase;
+        $flashcard->ukrainian_phrase = request()->ukrainian_phrase;
+    
+        $sound_file_path = request()->sound_file_path;
+    
+        //add leading slash to sound file path for easy use when accessing from db
+        if($sound_file_path[0] == "/") {
+    
+        } else {
+            $sound_file_path = "/" . $sound_file_path;
+        }
+    
+        $flashcard->sound_file_path = $sound_file_path;
+    
+        $flashcard->save();
+    
+        return redirect('/admin/flashcard/all');
+    }
+
+    public function index() {
+
+        $flashcards = Flashcard::all();
+        return view('create_flashcard', ['flashcards' => $flashcards]);
+    
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -59,41 +59,8 @@ Route::get('admin/grammar/all', [AdminGrammarController::class, 'index'])->middl
 
 Route::get('/admin/flashcard/create', [AdminFlashcardController::class, 'create'])->middleware(['auth']);
 Route::post('/admin/flashcard/delete', [AdminFlashcardController::class, 'destroy'])->middleware(['auth']);
-
-Route::post('/admin/flashcard/create', function() {
-
-    if (Auth::user()->id != 1) {
-        return redirect('/');
-    }
-
-    $flashcard = new Flashcard;
-
-    $flashcard->english_phrase = request()->english_phrase;
-    $flashcard->ukrainian_phrase = request()->ukrainian_phrase;
-
-    $sound_file_path = request()->sound_file_path;
-
-    //add leading slash to sound file path for easy use when accessing from db
-    if($sound_file_path[0] == "/") {
-
-    } else {
-        $sound_file_path = "/" . $sound_file_path;
-    }
-
-    $flashcard->sound_file_path = $sound_file_path;
-
-    $flashcard->save();
-
-    return redirect('/admin/flashcard/all');
-})->middleware(['auth']);;
-
-
-Route::get('/admin/flashcard/all', function() {
-
-    $flashcards = Flashcard::all();
-    return view('create_flashcard', ['flashcards' => $flashcards]);
-
-})->middleware(['auth']);;
+Route::post('/admin/flashcard/create', [AdminFlashcardController::class, 'store'])->middleware(['auth']);
+Route::get('/admin/flashcard/all', [AdminFlashcardController::class, 'index'])->middleware(['auth']);
 
 
 Route::get('flashcard-studying', function() {
