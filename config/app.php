6f4a8f6861daa881f7cdeb2d09bd15622e616f20
John <john@Johns-MacBook-Air.local>
2025-01-07 19:04:22 -0600

    added ignition

--- config/app.php
+++ config/app.php
@@ -139,6 +139,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Spatie\LaravelIgnition\IgnitionServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
