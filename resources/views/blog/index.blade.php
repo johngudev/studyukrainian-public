d8383c67a01b4b4c2e8fec455917f03c8b3f5a6a
John <johngucs@gmail.com>
2025-01-19 09:35:12 -0600

    run laravel mix

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,10 +1,27 @@
-here's some more bullshit
+@extends('layouts.layout')
 
+@section('title')
+ - Blog
+@endsection
+
+@section('css')
+<link href="css/extra-styles.css" rel="stylesheet">
+<style>
+    .navbar {
+        border: 1px solid lightgray;
+    }
+</style>
+@endsection
+
+
+@section('content')
 @antlers
-    <ul>
+    <ul class="mt-5">
 
     {{ collection:blog }}
-        <li>{{ title }}</li>
+        <li><a href="/blog/{{ slug }}">{{ title }}</a></li>
     {{ /collection:blog }}
     </ul>
 @endantlers
+
+@endsection
