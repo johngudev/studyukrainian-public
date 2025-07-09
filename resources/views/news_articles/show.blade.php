fb47eb556fee5230da95359b126e5b69c0aeb614
John <johngucs@gmail.com>
2025-07-09 12:29:54 -0500

    show ukrainian article

--- resources/views/news_articles/show.blade.php
+++ resources/views/news_articles/show.blade.php
@@ -91,9 +91,11 @@
 
         <div class="modern-panel p-3 p-md-5 text-graphite-black">
             <h1 class=""> {{$page->title}} </h1>
-
-            <div> {{ $page->ukrainian_article}}
-            </div>
         </div>
+
+        <div class="modern-panel p-3 p-md-5 text-graphite-black">
+            <h1 class=""> {!! $page->ukrainian_article !!} </h1>
+        </div>
+
     </div>
 @endsection
