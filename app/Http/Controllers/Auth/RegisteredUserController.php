c41ca3c7b5864692565e49cddae29cfb841b3b8d
John <johngucs@gmail.com>
2025-08-01 16:20:50 -0500

    add redirect for name field

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,14 +37,19 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+        if (!empty($request->input('name'))) {
+            // Honeypot
+            return redirect(RouteServiceProvider::HOME);
+        }
+
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            // 'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'name' => 'User',
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
